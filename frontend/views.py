b721ed623166d68115a6cf2dfc4e13999401df19
Mauro Marini <mauro.marini97@gmail.com>
2021-06-24 18:43:39 +0200

    fixed download url

--- frontend/views.py
+++ frontend/views.py
@@ -21,6 +21,11 @@ from api.document.querysets import document_queryset
 # ----------------------------------------------------------------------------------------------------------------------
 
 def document_list_view(request):
+    """
+    Document list page
+    :param request: the request
+    :return: the page
+    """
     document_list = document_queryset(request)  # api.document.queryset is permission filtered
     return render(request, 'document_list_page.html', {'documents': document_list})
 
@@ -32,6 +37,12 @@ def document_list_view(request):
 # ----------------------------------------------------------------------------------------------------------------------
 
 def document_versions_list_view(request, document_id):
+    """
+    Document detail and version list page
+    :param request: the request
+    :param document_id: the id of the document
+    :return: the page
+    """
     queryset = document_queryset(request)  # api.document.queryset is permission filtered
     exists = queryset.filter(id=document_id).exists()
     if exists:
@@ -42,6 +53,12 @@ def document_versions_list_view(request, document_id):
 
 
 def document_version_detail_view(request, version_id):
+    """
+    Version detail page
+    :param request: the request
+    :param version_id: the id of the document version
+    :return: the page
+    """
     queryset = document_queryset(request)  # api.document.queryset is permission filtered
     exists = DocumentVersion.objects.filter(id=version_id).exists()
     if exists:
@@ -49,7 +66,9 @@ def document_version_detail_view(request, version_id):
         if queryset.filter(id=version.document.id).exists():  # security check
             is_last = version == \
                       DocumentVersion.objects.filter(document=version.document).order_by('-creation_timestamp')[0]
-            return render(request, 'document_version_detail_page.html', {'version': version, 'is_last': is_last})
+            file_name = version.file_resource.name
+            return render(request, 'document_version_detail_page.html',
+                          {'version': version, 'is_last': is_last, 'file_name': file_name})
     return error_404(request)
 
 
@@ -60,6 +79,11 @@ def document_version_detail_view(request, version_id):
 # ----------------------------------------------------------------------------------------------------------------------
 
 def favorites_list_view(request):
+    """
+   Favorite list page
+   :param request: the request
+   :return: the page
+   """
     if request.user is not None:
         favorites = Favorite.objects.filter(citizen__username=request.user.username)
         document_list = document_queryset(request).filter(id__in=[f.document.id for f in favorites])
