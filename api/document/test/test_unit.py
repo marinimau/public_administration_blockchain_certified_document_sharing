2c1870d28d9bfaf1710c800b5817c34b91e2d2ea
Mauro Marini <mauro.marini97@gmail.com>
2021-07-09 18:15:58 +0200

    update permission tests

--- api/document/test/test_unit.py
+++ api/document/test/test_unit.py
@@ -98,3 +98,155 @@ class DocumentUnitTest(DocumentTestAbstract, TestCase):
         request.user = None
         result = is_citizen.has_permission(request, None)
         self.assertFalse(result)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    #   isOwner
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def test_permission_is_owner_permission_ok(self):
+        """
+        Testing "isOwner" custom permission (ok)
+        :return:
+        """
+        is_owner = IsOwner()
+        request = HttpRequest()
+        request.user = self.citizens[0]
+        obj = self.permissions[0]
+        result = is_owner.has_object_permission(request, None, obj)
+        self.assertTrue(result)
+
+    def test_permission_is_owner_permission_fail_different_citizen(self):
+        """
+        Testing "isOwner" custom permission (fail)
+        :return:
+        """
+        is_owner = IsOwner()
+        request = HttpRequest()
+        request.user = self.citizens[1]
+        obj = self.permissions[0]
+        result = is_owner.has_object_permission(request, None, obj)
+        self.assertFalse(result)
+
+    def test_permission_is_owner_permission_fail_different_operator(self):
+        """
+        Testing "isOwner" custom permission (fail)
+        :return:
+        """
+        is_owner = IsOwner()
+        request = HttpRequest()
+        request.user = self.pa_operators[0]
+        obj = self.permissions[0]
+        result = is_owner.has_object_permission(request, None, obj)
+        self.assertFalse(result)
+
+    def test_permission_is_owner_favorite_ok(self):
+        """
+        Testing "isOwner" custom permission (ok)
+        :return:
+        """
+        is_owner = IsOwner()
+        request = HttpRequest()
+        request.user = self.citizens[0]
+        obj = self.favorites[0]
+        result = is_owner.has_object_permission(request, None, obj)
+        self.assertTrue(result)
+
+    def test_permission_is_owner_favorite_fail_different_citizen(self):
+        """
+        Testing "isOwner" custom permission (fail)
+        :return:
+        """
+        is_owner = IsOwner()
+        request = HttpRequest()
+        request.user = self.citizens[1]
+        obj = self.favorites[0]
+        result = is_owner.has_object_permission(request, None, obj)
+        self.assertFalse(result)
+
+    def test_permission_is_owner_favorite_fail_different_operator(self):
+        """
+        Testing "isOwner" custom permission (fail)
+        :return:
+        """
+        is_owner = IsOwner()
+        request = HttpRequest()
+        request.user = self.pa_operators[0]
+        obj = self.favorites[0]
+        result = is_owner.has_object_permission(request, None, obj)
+        self.assertFalse(result)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    #   DocumentPermissions
+    # ------------------------------------------------------------------------------------------------------------------
+
+    def test_document_permission_post_ok(self):
+        """
+        Testing "DocumentPermission" custom permission - post (ok)
+        :return:
+        """
+        document_permissions = DocumentPermissions()
+        request = HttpRequest()
+        request.method = 'POST'
+        request.user = self.pa_operators[0]
+        result = document_permissions.has_permission(request, None)
+        self.assertTrue(result)
+
+    def test_document_permission_post_fail_is_citizen(self):
+        """
+        Testing "DocumentPermission" custom permission - post (fail, only an operator can create a document)
+        :return:
+        """
+        document_permissions = DocumentPermissions()
+        request = HttpRequest()
+        request.method = 'POST'
+        request.user = self.citizens[0]
+        result = document_permissions.has_permission(request, None)
+        self.assertFalse(result)
+
+    def test_document_permission_post_fail_no_auth(self):
+        """
+        Testing "DocumentPermission" custom permission - post (fail, only an operator can create a document)
+        :return:
+        """
+        document_permissions = DocumentPermissions()
+        request = HttpRequest()
+        request.method = 'POST'
+        request.user = None
+        result = document_permissions.has_permission(request, None)
+        self.assertFalse(result)
+
+    def test_document_permission_get_no_auth(self):
+        """
+        Testing "DocumentPermission" custom permission - get (ok)
+        :return:
+        """
+        document_permissions = DocumentPermissions()
+        request = HttpRequest()
+        request.method = 'GET'
+        request.user = None
+        result = document_permissions.has_permission(request, None)
+        self.assertTrue(result)
+
+    def test_document_permission_get_citizen_auth(self):
+        """
+        Testing "DocumentPermission" custom permission - get (ok)
+        :return:
+        """
+        document_permissions = DocumentPermissions()
+        request = HttpRequest()
+        request.method = 'GET'
+        request.user = self.citizens[0]
+        result = document_permissions.has_permission(request, None)
+        self.assertTrue(result)
+
+    def test_document_permission_get_operator_auth(self):
+        """
+        Testing "DocumentPermission" custom permission - get (ok)
+        :return:
+        """
+        document_permissions = DocumentPermissions()
+        request = HttpRequest()
+        request.method = 'GET'
+        request.user = self.pa_operators[0]
+        result = document_permissions.has_permission(request, None)
+        self.assertTrue(result)
