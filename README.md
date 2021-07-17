afc79d9af16c3a007bff5fee0f1db9f847f70340
Mauro Marini <mauro.marini97@gmail.com>
2021-07-17 10:08:23 +0200

    finish tests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)
+
 # Public Administration Blockchain Certified Document Sharing
 
 A dApp to distribute blockchain certified documents from PAs to citizens. All the features are accessible via REST api.
--- contracts/__init__.py
+++ contracts/__init__.py
@@ -1,8 +1,24 @@
-#
-#   public_administration_blockchain_certified_document_sharing copyright © 2021 - all rights reserved
-#   Created at: 05/07/21
-#   By: mauromarini
-#   License: MIT
-#   Repository: https://github.com/marinimau/public_administration_blockchain_certified_document_sharing
-#   Credits: @marinimau (https://github.com/marinimau)
-#
+"""
+This software is distributed under MIT/X11 license
+
+Copyright (c) 2021 Mauro Marini - University of Cagliari
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
